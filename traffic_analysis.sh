e17dd59fc140fc242c57f130e86c7e7232cf1ce3
наме-ru <132847268+HAME-RU@users.noreply.github.com>
2023-06-11 22:52:06 -0600

    Update traffic_analysis.sh

--- traffic_analysis.sh
+++ traffic_analysis.sh
@@ -24,10 +24,10 @@ run_traffic_analysis() {
     tshark -i eth0 -l -T fields -f "udp" -e ip.dst -Y "$filter" -Y "ip.dst!=192.168.0.0/16 and ip.dst!=10.0.0.0/8 and ip.dst!=172.16.0.0/12" | while read -r line; do
         whois "$line" > /tmp/b
 
-        filter=$(cat /tmp/b | xargs | egrep -iv "facebook|google" | wc -l)
+        filter=$(grep -ivE "facebook|google" /tmp/b | wc -l)
 
         if [ "$filter" -gt 0 ]; then
-            targetinfo=$(cat /tmp/b | egrep -iw "OrgName:|NetName:|Country:")
+            targetinfo=$(grep -iw "OrgName:\|NetName:\|Country:" /tmp/b)
             echo "$line --- $targetinfo"
         fi
     done
